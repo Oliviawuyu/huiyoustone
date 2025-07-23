d93f71ddc8ef3e6631ba78b56c64610ed98f426c
Oliviawuyu <s2993856@gmail.com>
2025-07-23 14:56:03 +0800

    Updates

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 生產環境配置檔案
 // 這個檔案會在 HTML 中載入，設定全域變數
-window.GOOGLE_MAPS_API_KEY = 'AIzaSyClBvNM03tATjAMvWAAUP_2eHRBMFzr0dY'; 
+window.GOOGLE_MAPS_API_KEY = 'AIzaSyDXADttp8KmOfoegvQNSDXuqMSGue43Paw'; 
